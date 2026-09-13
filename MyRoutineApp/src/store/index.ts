1a0899c08964573656b182680d860cf4d0814643
Jesus Alexander <jesus_matute@unitec.edu>
2026-09-12 22:34:29 -0600

    App.tsx - se elimino SkincareProvider ya que el estado migro a Redux

--- MyRoutineApp/src/store/index.ts
+++ MyRoutineApp/src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userProfileReducer from "./slices/userProfileSlice";
+import skincareReducer from './slices/SkincareSlice'; 
 
 export const store = configureStore ({
     reducer: {
-        userProfile: userProfileReducer
+        userProfile: userProfileReducer,
+        skincare: skincareReducer
     }, 
 });
 
